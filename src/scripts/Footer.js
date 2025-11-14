ed4814b55a4bc6ba6591fb4bf13a3aa81c963b9a
Alejandro Delgado <alejandro.delgado@ucsp.edu.pe>
2025-11-14 02:12:41 -0500

    Add pre-push event

---
+++ src/scripts/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <hr className="mt-5 mb-4" />
+      <p className="text-muted">Created for practice purposes. Article Web.</p>
+    </footer>
+  );
+}
+
+export default Footer;
